3b2b14e4b841f3fefbb76bff0d8be50477305d8f
Gaoussou <koita.gaouss@gmail.com>
2022-09-19 09:41:11 +0000

    First Commit

---
+++ src/main/java/ml/pic/tech/app/alimentation/AlimentationApplication.java
@@ -0,0 +1,13 @@
+package ml.pic.tech.app.alimentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/pic/tech/app/alimentation/AlimentationApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.pic.tech.app.alimentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
